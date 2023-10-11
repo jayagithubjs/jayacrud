c5d5ed75702a144239a42554921d9e78f3bbff6a
jayagithubjs <145575665+jayagithubjs@users.noreply.github.com>
2023-10-11 11:16:26 +0530

    Add files via upload

---
+++ phpcrudajax/playerstable.php
@@ -0,0 +1,17 @@
+<!-- table -->
+<table class="table" id="userstable">
+  <thead>
+    <tr>
+    
+      <th scope="col">Picture</th>
+      <th scope="col">Name&Case name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+<!-- table -->
